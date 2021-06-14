00a06e13da275a9035cdad626694fd2fae482e55
eNV25 <61089994+eNV25@users.noreply.github.com>
2021-06-14 13:37:52 +0200

    Add build.sh script

--- cmd/printmpd/main.go
+++ cmd/printmpd/main.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"fmt"
-	"reflect"
-	"runtime"
 
 	"github.com/env25/mpdlrc/internal/client"
 	"github.com/env25/mpdlrc/internal/mpd"
 )
-
-func FunctionName(i interface{}) string {
-	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
-}
-
-func handle(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	var c client.Client = mpd.NewMPDClient("unix", "/run/user/1000/mpd/socket")
