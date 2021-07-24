2616cf15af258bbb817479f9d28c255f2636055d
eNV25 <61089994+eNV25@users.noreply.github.com>
2021-07-24 18:20:50 +0200

    Use github.com/rivo/uniseg for combining marks, etc.

--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module github.com/env25/mpdlrc
 
 go 1.16
 
-// replace version with `master`
-// then run `go mod tidy`
-require github.com/gdamore/tcell/v2 v2.4.1-0.20210717182757-0b75e1e09e39
+require (
+	// replace version with `master`
+	// then run `go mod tidy`
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210717182757-0b75e1e09e39
+	github.com/neeharvi/textwidth v0.0.0-20210724160824-a34d021a0578
+)
 
 require (
 	github.com/fhs/gompd/v2 v2.2.0
-	github.com/neeharvi/textwidth v0.0.0-20210722130453-e23a60b2b575
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.3
 	github.com/rivo/uniseg v0.1.0
 	github.com/spf13/pflag v1.0.5
--- internal/lyricswidget.go
+++ internal/lyricswidget.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gdamore/tcell/v2"
 	"github.com/gdamore/tcell/v2/views"
 	"github.com/neeharvi/textwidth"
+	"github.com/rivo/uniseg"
 	"golang.org/x/text/unicode/norm"
 
 	"github.com/env25/mpdlrc/internal/status"
@@ -101,6 +102,7 @@ func (w *LyricsWidget) updateModel(lines []string, index int) {
 	m := &lyricsModel{}
 
 	x, y := w.view.Size()
+	mid := y / 2
 
 	m.width = 0
 	m.height = y + 1
@@ -111,8 +113,8 @@ func (w *LyricsWidget) updateModel(lines []string, index int) {
 	m.styles = make([][]tcell.Style, m.height)
 	m.cells = make([]int, m.height)
 
-	i1 := (index - (y / 2))
-	i2 := (index + (y / 2)) + 1
+	i1 := index - mid
+	i2 := index + mid + 1
 
 	row := 0
 
@@ -122,18 +124,18 @@ func (w *LyricsWidget) updateModel(lines []string, index int) {
 
 	for i := i1; i < i2 && i < len(lines); i++ {
 		n := textwidth.WidthString(lines[i])
-		off := ((x - n) / 2) + 1
+		off := (x-n)/2 + 1
 		if off < 0 {
-			off = 1
+			off = 0
 		}
 		n += off
-
-		cell := 0
 
 		m.maincs[row] = make([]rune, n)
 		m.combcs[row] = make([][]rune, n)
 		m.widths[row] = make([]int, n)
 		m.styles[row] = make([]tcell.Style, n)
+
+		cell := 0
 
 		for ; off > 0; off-- {
 			m.maincs[row][cell] = ' '
@@ -141,17 +143,17 @@ func (w *LyricsWidget) updateModel(lines []string, index int) {
 			cell += 1
 		}
 
-		for _, r := range lines[i] {
-			w := textwidth.WidthRune(r)
+		graphemes := uniseg.NewGraphemes(lines[i])
 
-			m.maincs[row][cell] = r
-			m.widths[row][cell] = w
+		for graphemes.Next() {
+			runes := graphemes.Runes()
+			wd := textwidth.WidthRunes(runes)
 
-			if row == (y / 2) {
-				m.styles[row][cell] = tcell.StyleDefault.Bold(true).Reverse(true)
-			}
+			m.maincs[row][cell] = runes[0]
+			m.combcs[row][cell] = runes[1:]
+			m.widths[row][cell] = wd
 
-			cell += w
+			cell += wd
 		}
 
 		m.cells[row] = cell
@@ -163,7 +165,42 @@ func (w *LyricsWidget) updateModel(lines []string, index int) {
 		row++
 	}
 
+	{
+		spc := true
+		style := tcell.StyleDefault.Bold(true).Reverse(true)
+		for i, r := range m.maincs[mid] {
+			if spc && !isSpace(r) {
+				spc = false
+			}
+			if !spc {
+				m.styles[mid][i] = style
+			}
+		}
+	}
+
 	w.cellView.SetModel(m)
+}
+
+func isSpace(r rune) bool {
+	if r <= '\u00FF' {
+		// Obvious ASCII ones: \t through \r plus space. Plus two Latin-1 oddballs.
+		switch r {
+		case ' ', '\t', '\n', '\v', '\f', '\r':
+			return true
+		case '\u0085', '\u00A0':
+			return true
+		}
+		return false
+	}
+	// High-valued ones.
+	if '\u2000' <= r && r <= '\u200A' {
+		return true
+	}
+	switch r {
+	case '\u1680', '\u2028', '\u2029', '\u202F', '\u205F', '\u3000':
+		return true
+	}
+	return false
 }
 
 var _ views.CellModel = &lyricsModel{}
