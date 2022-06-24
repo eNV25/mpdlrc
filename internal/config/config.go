556ed25f431808d817d74b1d4d0f5055db64aa70
eNV25 <env252525@gmail.com>
2022-06-24 08:08:20 +0400

    config: use values from $MPD_HOST $MPD_PORT

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,10 +22,35 @@ func DefaultConfig() (cfg *Config) {
 	cfg = &Config{}
 	cfg.MusicDir = "~/Music"
 	cfg.LyricsDir = ""
-	cfg.MPD.Connection = "tcp"
-	cfg.MPD.Address = "localhost:6600"
-	cfg.MPD.Password = ""
-	return cfg
+	host := os.Getenv("MPD_HOST")
+	port := os.Getenv("MPD_PORT")
+	if host == "" {
+		cfg.MPD.Connection = "tcp"
+		cfg.MPD.Address = "localhost:6600"
+		cfg.MPD.Password = ""
+	} else {
+		// If @ is found in host, it has two possible meanings.
+		// @path          : At the beginning, this is a Linux abstract socket path.
+		// password@host  : Elsewhere, it's the password and host.
+		// password@@path : Using @@ probably means both password and abstract socket,
+		//                  but we don't need to handle this explicitly.
+		// Summarised from docs of --host option in man:mpc(1).
+		password := ""
+		at := strings.Index(host, "@")
+		if at > 0 {
+			password, host = host[:at], host[at+1:]
+		}
+		if port == "" || strings.Contains(host, string(os.PathSeparator)) {
+			cfg.MPD.Connection = "unix"
+			cfg.MPD.Address = host
+			cfg.MPD.Password = password
+		} else {
+			cfg.MPD.Connection = "tcp"
+			cfg.MPD.Address = host + ":" + port
+			cfg.MPD.Password = password
+		}
+	}
+	return
 }
 
 // Expand expands tilde ("~") and variables ("$VAR" or "${VAR}") in paths in Config.
